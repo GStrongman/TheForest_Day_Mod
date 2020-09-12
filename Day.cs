167ccd660cca60eae4f894a3d9b990b7ae34a069
GStrongman <georgia.strongman@gmail.com>
2020-09-12 20:19:59 +1200

    Create namespace, setup project

---
+++ Day.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ModAPI.Attributes;
+using TheForest.Utils;
+using UnityEngine;
+
+namespace Day
+{
+    class Day : MonoBehaviour
+    {
+        [ExecuteOnGameStart]
+        private static void AddMeToScene()
+        {
+            new GameObject("__Day__").AddComponent<Day>();
+        }
+    }
+}
+
